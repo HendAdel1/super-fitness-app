be5126cd4beb12055092450a6da6faebd71e7ccf
HendAdel1 <1hend.adel1@gmail.com>
2026-09-26 02:48:11 +0300

    add missing component files, solve tsconfig ooutDir bug

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.html',
+  styleUrl: './login.scss',
 })
 export class Login {}
--- src/app/features/auth/pages/register/register.ts
+++ src/app/features/auth/pages/register/register.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.html',
+  styleUrl: './register.scss',
 })
 export class Register {}
